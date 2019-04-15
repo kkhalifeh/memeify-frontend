2335875f835b366cf46e05cad4e44fad482e5588
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-04-15 13:55:16 -0400

    rendered captions for meme

--- src/main.js
+++ src/main.js
@@ -4,8 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
 //Get elements
 const container = document.querySelector('.container')
-
+const memeCard = document.querySelector('.card')
 const memes = []
+
+//Event listeners
+
+//Click on next meme
+container.addEventListener('click', nextMeme);
+
+
 //Get memes
 function getMemes(){
   fetch('http://localhost:3000/memes')
@@ -14,37 +21,77 @@ function getMemes(){
     data.forEach((meme) => {
       memes.push(meme)
     })
-    renderMemes()
+    renderMeme()
   })
 }
 
-function renderMemes() {
+
+//Render one random meme
+function renderMeme() {
   let output = ''
-  memes.forEach((meme) => {
-    console.log('meme', meme);
-    output +=
-    `
-    <div class="card mb-3">
-      <h3 class="card-header">${meme.title}</h3>
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <h6 class="card-subtitle text-muted">Support card subtitle</h6>
-      </div>
-      <img style="height: 70%; width: 70%; display: block;" src="${meme.url}" alt="Card image">
-      <div class="card-body">
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-      <div class="card-body">
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
-      </div>
-      <div class="card-footer text-muted">
-        2 days ago
-      </div>
+  let n = getRandomInt(memes.length)
+  console.log('n', n);
+  output +=
+  `
+  <div class="card mb-3" data-id="${memes[n].id}">
+    <h3 class="card-header">${memes[n].title}</h3>
+    <div class="card-body">
+      <h5 class="card-title">Special title treatment</h5>
+      <h6 class="card-subtitle text-muted">Support card subtitle</h6>
     </div>
-    `
-    container.innerHTML = output
+    <img style="height: 70%; width: 70%; display: block;" src="${memes[n].url}" alt="Card image">
+    <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <ul class="list-group">
+      </ul>
+    </div>
+    <div class="card-body">
+      <button type="button" class="btn btn-success next-meme">Next Meme</button>
+      <button type="button" class="btn btn-success">Add Caption</button>
+      <button type="button" class="btn btn-success">Like</button>
+    </div>
+    <div class="card-footer text-muted">
+      2 days ago
+    </div>
+  </div>
+  `
+  container.innerHTML = output
+  memeCaptions(memes[n])
+}
+
+//Render caption for meme
+function memeCaptions(meme) {
+  const listGroup = document.querySelector('.list-group')
+  console.log(listGroup.innerHTML);
+  // debugger
+  meme.captions.forEach((caption) => {
+    const li = document.createElement('li')
+    const span = document.createElement('span')
+    li.className = "list-group-item d-flex justify-content-between align-items-center"
+    li.innerHTML = caption.text
+
+    span.className = "badge badge-primary badge-pill"
+    span.innerHTML = caption.likes
+
+    li.appendChild(span)
+
+    listGroup.appendChild(li)
   })
+
+}
+
+//Get random number
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+//Render next meme
+function nextMeme(e) {
+  e.preventDefault()
+  if (e.target.className === 'btn btn-success next-meme') {
+    console.log('e.target', e.target);
+    renderMeme()
+  }
 }
 
 getMemes()
