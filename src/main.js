6e9b1eeb2054c83bfa94020456d2d1edebe004f2
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-04-15 11:55:10 -0400

    basic rendering of memes on index page

--- src/main.js
+++ src/main.js
@@ -1,17 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Content Loaded');;
-
-
 });
 
-getMemes()
+//Get elements
+const container = document.querySelector('.container')
 
+const memes = []
+//Get memes
 function getMemes(){
   fetch('http://localhost:3000/memes')
   .then(resp => resp.json())
-  .then(function(memes){
-    memes.forEach(function(meme){
-      console.log(meme.url)
+  .then((data) => {
+    data.forEach((meme) => {
+      memes.push(meme)
     })
+    renderMemes()
   })
 }
+
+function renderMemes() {
+  let output = ''
+  memes.forEach((meme) => {
+    console.log('meme', meme);
+    output +=
+    `
+    <div class="card mb-3">
+      <h3 class="card-header">${meme.title}</h3>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <h6 class="card-subtitle text-muted">Support card subtitle</h6>
+      </div>
+      <img style="height: 70%; width: 70%; display: block;" src="${meme.url}" alt="Card image">
+      <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <div class="card-body">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+      <div class="card-footer text-muted">
+        2 days ago
+      </div>
+    </div>
+    `
+    container.innerHTML = output
+  })
+}
+
+getMemes()
